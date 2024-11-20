8214d613ad25b814d980799856157b1ade58e6de
noyonalways <noyonrahman2003@gmail.com>
2024-11-21 00:03:45 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import ShinyButton from "@/components/ui/shiny-button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="flex items-center justify-center h-screen">
+        <div className="text-center space-y-2">
+          <h1 className="text-2xl md:text-3xl font-bold">
+            Portfolio 2.0 Admin Panel
+          </h1>
+          <p>Under Construction</p>
+          <ShinyButton>Learn More</ShinyButton>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
